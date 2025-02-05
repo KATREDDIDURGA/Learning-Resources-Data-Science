12e2be572631632a077bd8091b7881cc2702af8d
Katreddi Sri Sai Durga <72980202+KATREDDIDURGA@users.noreply.github.com>
2025-02-04 19:26:46 -0600

    Add files via upload

---
+++ SQL COurse.sql
@@ -0,0 +1,17 @@
+SELECT * FROM moviesdb.movies;
+
+Select title, industry from moviesdb.movies;
+
+Use moviesdb;
+
+Select title, industry from movies where industry="Bollywood";
+
+Select count(*) from movies where industry="Bollywood";
+
+select distinct industry from movies;
+
+Select * from movies where title like "%thor%";
+
+Select * from movies where studio="";
+
+
